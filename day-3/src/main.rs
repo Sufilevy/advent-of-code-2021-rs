0be73f412a8889fe049d044996a5ae6e28d8e0f9
Sufilevy <sufilevy@gmail.com>
2022-09-10 20:59:19 +0300

    Finished day-3 part 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,10 +1,9 @@
-#![feature(drain_filter)]
-
 use std::{cmp::Ordering, fs, io::Error};
 
 fn main() -> Result<(), Error> {
     let input = fs::read_to_string("input.txt")?;
-    let input = input.split('\n').collect::<Vec<&str>>();
+    let mut input = input.split('\n').collect::<Vec<&str>>();
+    input.pop();
     println!("{}", puzzle_one(&input));
     print!("{}", puzzle_two(&input));
     Ok(())
@@ -18,7 +17,7 @@ fn puzzle_one(input: &[&str]) -> i32 {
         let mut appearances = (0, 0);
         input
             .iter()
-            .for_each(|line| match line.chars().nth(11 - i).unwrap_or(' ') {
+            .for_each(|line| match line.chars().nth(11 - i).unwrap() {
                 '1' => appearances.1 += 1,
                 '0' => appearances.0 += 1,
                 _ => {}
@@ -34,40 +33,33 @@ fn puzzle_one(input: &[&str]) -> i32 {
 }
 
 fn puzzle_two(input: &[&str]) -> i32 {
-    let mut ogr = input.to_owned().clone(); // Oxygen generator rating
-    let mut co2sr = input.to_owned().clone(); // CO2 scrubber rating
+    let filter_rating = |msb, lsb| {
+        let mut vec = input.to_owned().clone();
+        let mut i = 0;
+        while vec.len() > 1 {
+            let mut appearances = (0, 0);
+            vec.iter()
+                .for_each(|line| match line.chars().nth(i).unwrap() {
+                    '1' => appearances.1 += 1,
+                    '0' => appearances.0 += 1,
+                    _ => {}
+                });
 
-    for i in 0..12 {
-        let mut appearances = (0, 0);
-        input
-            .iter()
-            .for_each(|line| match line.chars().nth(11 - i).unwrap_or(' ') {
-                '1' => appearances.1 += 1,
-                '0' => appearances.0 += 1,
-                _ => {}
-            });
+            let filter = match appearances.1.cmp(&appearances.0) {
+                Ordering::Greater => msb,
+                Ordering::Equal => msb,
+                Ordering::Less => lsb,
+            };
 
-        let (ogr_filter, co2sr_filter) = match appearances.0.cmp(&appearances.1) {
-            Ordering::Less => ('1', '0'),
-            Ordering::Equal => ('1', '0'),
-            Ordering::Greater => ('0', '1'),
-        };
-
-        if ogr.len() > 1 {
-            ogr = ogr
-                .iter()
-                .filter(|num| num.to_owned().chars().nth(11 - i).unwrap_or(' ') == ogr_filter)
-                .map(|e| e.to_owned())
-                .collect();
+            vec.retain(|num| num.to_owned().chars().nth(i).unwrap() == filter);
+            i += 1;
         }
-        if co2sr.len() > 1 {
-            co2sr = co2sr
-                .iter()
-                .filter(|num| num.to_owned().chars().nth(11 - i).unwrap_or(' ') == co2sr_filter)
-                .map(|e| e.to_owned())
-                .collect();
-        }
-    }
 
-    i32::from_str_radix(ogr[0], 2).unwrap() * i32::from_str_radix(co2sr[0], 2).unwrap()
+        vec[0].to_string().trim_end_matches('\r').to_owned()
+    };
+
+    let ogr = filter_rating('1', '0'); // Oxygen generator rating
+    let co2sr = filter_rating('0', '1'); // CO2 scrubber rating
+
+    i32::from_str_radix(&ogr, 2).unwrap() * i32::from_str_radix(&co2sr, 2).unwrap()
 }
