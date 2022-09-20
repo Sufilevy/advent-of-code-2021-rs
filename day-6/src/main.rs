d6eb9192b376079493c157498f18af3db12cde9c
Sufilevy <sufilevy@gmail.com>
2022-09-20 10:29:27 +0300

    Finished day-6 part 2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -3,7 +3,7 @@ use std::{fs, io::Error};
 fn main() -> Result<(), Error> {
     let input = fs::read_to_string("input.txt")?;
     let input = input.split(',').collect::<Vec<_>>();
-    // println!("{}", puzzle_one(&input));
+    println!("{}", puzzle_one(&input));
     println!("{}", puzzle_two(&input));
     Ok(())
 }
@@ -28,13 +28,21 @@ fn puzzle_one(input: &[&str]) -> i32 {
     fish.len() as i32
 }
 
-fn puzzle_two(input: &[&str]) -> i32 {
-    let mut fish = input
+fn puzzle_two(input: &[&str]) -> i64 {
+    let fish = input
         .iter()
-        .map(|f| f.trim().parse::<i32>().unwrap())
+        .map(|f| f.trim().parse::<i64>().unwrap())
         .collect::<Vec<_>>();
-    for day in 0..80 {
-        println!("{day}");
+    let mut num_of_fish = vec![0_i64; 9];
+
+    for f in fish {
+        num_of_fish[f as usize] += 1;
     }
-    fish.len() as i32
+
+    for _ in 0..256 {
+        num_of_fish.rotate_left(1);
+        num_of_fish[6] += num_of_fish[8];
+    }
+
+    num_of_fish.iter().sum::<i64>()
 }
